f074a9839867532f307b587a6283088aa644f974
SohamAgawane <93815863+SohamAgawane@users.noreply.github.com>
2025-02-25 21:31:48 +0530

    Implement JWT authentication with secure token handling

--- controllers/url.js
+++ controllers/url.js
@@ -11,6 +11,7 @@ async function handleGenerateShortURL(req, res) {
         shortId: shortID,
         redirectURL: body.url,
         visitHistory: [],
+        createdBy: req.user._id,
     });
 
     return res.render("home", { id : shortID });
